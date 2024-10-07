9097c93b7f2ad84d44bb6fed1fce56ce2a5b15e2
gehcosta <geovannehc@gmail.com>
2024-10-07 18:05:17 -0300

    Inicio da pagina homePage

--- app/Access/pageAccess.js
+++ app/Access/pageAccess.js
@@ -2,18 +2,23 @@
 // Se ele não tiver acesso, vou redirecionar ele para a página de login
 // O redirecionamento das outras páginas será feito no arquivo de configuração do sistema
 console.log("hello");
-function homeAccess() {
-    
-    const pagePath = "../wwwroot/pages/homePage.html";
-    fetch(pagePath)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('main').innerHTML = data;
-        })
-        .catch(error => console.error('Erro ao carregar a página:', error));
-}
+// function homeAccess() {
+//    
+//     const pagePath = "../wwwroot/pages/homePage.html";
+//     fetch(pagePath)
+//         .then(response => response.text())
+//         .then(data => {
+//             document.getElementById('main').innerHTML = data;
+//         })
+//         .catch(error => console.error('Erro ao carregar a página:', error));
+// }
 
+function homeAccess() {
+    const pagePath = "../wwwroot/pages/homePage.html";
+    window.location.href = pagePath;
+}
 
 // Do login vou receber o token e o id do usuário, que eu vou salvar no localStorage
 
 // Adicionar também o logout, que vai remover o token e o id do usuário do localStorage
+
