ff7e136d3c838ada93ca7d28459db16ac9327ae3
Geovanne Costa <geovannehc@gmail.com>
2024-10-19 00:25:09 -0300

    corrigido a abertura e fechamento da sideBar

--- wwwroot/js/sideBar.js
+++ wwwroot/js/sideBar.js
@@ -2,28 +2,44 @@ const sideBar = document.getElementById('sideBar');
 const hamburguer = document.getElementById('hamburguer');
 
 let isExpanded = false;
+let isAnimating = false;
+let buttonAnimating = false; 
 
-const originalWidth = '3.2rem'; 
-
-const expandedWidth = '16rem'; 
+const originalWidth = '3.2rem';
+const expandedWidth = '16rem';
 
 hamburguer.addEventListener('click', function() {
+    if (isAnimating || buttonAnimating) return;
+
+    isAnimating = true; 
+
     if (isExpanded) {
-        setTimeout(() => {
-            sideBar.style.width = originalWidth;
-        }, 250);
+        sideBar.style.width = originalWidth;
+
+        buttonAnimating = true;
         for (let i = 0; i <= 3; i++) {
-            document.getElementById(`page[${i}]`).style.width = '3.2rem';
+                document.getElementById(`page[${i}]`).style.width = '3.2rem';
+                if (i === 3) {
+                    buttonAnimating = false; 
+                }
         }
-        
     } else {
         sideBar.style.width = expandedWidth;
+
+        buttonAnimating = true;
         for (let i = 0; i <= 3; i++) {
             setTimeout(() => {
                 document.getElementById(`page[${i}]`).style.width = '16rem';
-            }, 300 * i);
+                if (i === 3) {
+                    buttonAnimating = false;
+                }
+            }, 100 * i);
         }
     }
-    // Inverte o estado
+
     isExpanded = !isExpanded;
+
+    setTimeout(() => {
+        isAnimating = false; 
+    }, 300); 
 });
